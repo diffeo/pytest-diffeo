005ac283d00194d48e6dd1265b26601dd7174d86
John R. Frank <jrf@diffeo.com>
2014-11-06 19:53:38 +0000

    adding pypi classifiers; fixing license string for listing manifests

--- setup.py
+++ setup.py
@@ -14,12 +14,18 @@ setup(name='pytest-diffeo',
       version=VERSION,
       description='Common py.test support for Diffeo packages',
       url='https://github.com/diffeo/pytest-diffeo',
+      license='MIT/X11 license http://opensource.org/licenses/MIT',
       long_description=read_file('README.rst'),
       author='Diffeo, Inc.',
       author_email='support@diffeo.com',
       packages=find_packages(),
       include_package_data=True,
       install_requires=['pytest'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',  ## MIT/X11 license http://opensource.org/licenses/MIT
+      ],
       entry_points={
           'pytest11': [
               'diffeo = pytest_diffeo',
